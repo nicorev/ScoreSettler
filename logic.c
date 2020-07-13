dab90b4496017f1b002c4600446f9a52445b5d0d
andreLen <andrelenoirc@gmail.com>
2020-07-13 19:35:34 -0400

    Fixed various bugs and added new features

--- logic.c
+++ logic.c
@@ -6,7 +6,6 @@
 
 typedef struct Opt
 {
-    //dynamically allocated name
     char *name;
     double odds;
 }opt;
@@ -20,52 +19,70 @@ typedef struct Node
 
  struct 
 {
-    //is a linked list
     struct Node *head;
     int evenOdds;
     int remainingOdds; 
     int opts ; 
 } wheel;
 
-void setEvenOdds()
+void set_even_odds()
 {
     double odds = 100 / wheel.opts;
     struct Node *cur = wheel.head;
-    while (cur =! NULL)
+    while (cur != NULL)
     {
         cur->data->odds = odds;
+        cur = cur->next;
     }   
 }
 
-void add(char name[100], double odds)
+void add_opt(char name[100], double odds)
 {
-    wheel.opts++;
     struct Node *cur = wheel.head;
     struct Node *prev;
-    if (cur != NULL)
+    wheel.opts++;
+    if (cur == NULL)
     {
-        printf("here");
-        while (cur->next != NULL)
+        cur = malloc(sizeof(node));
+        cur->data = malloc(sizeof(opt));
+        cur->data->name = malloc(strlen(name) * sizeof(char) + 1);
+        strcpy(cur->data->name, name);
+        wheel.head = cur;
+        if (wheel.evenOdds)
+         {
+             set_even_odds();
+         }
+         else
+         {
+             cur->data->odds = odds;
+             wheel.remainingOdds -= odds;
+         } 
+         
+    }
+    else
+    {
+        while(cur->next != NULL)
         {
             cur = cur->next;
         }
-        prev = cur;
         cur->next = malloc(sizeof(node));
+        prev = cur;
         cur = cur->next;
         cur->prev = prev;
-    }   
-    else 
-        cur = malloc(sizeof(node));
-
-    cur->data = malloc(sizeof(opt));
-    cur->data->name = malloc(strlen(name)*sizeof(char));
-    if (wheel.evenOdds)
-        setEvenOdds();
-    else 
-        {
-            cur->data->odds = odds;
-            wheel.remainingOdds -= odds;
-        }   
+        cur->data = malloc(sizeof(opt));
+        cur->data->name = malloc(strlen(name) * sizeof(char) + 1);
+        strcpy(cur->data->name, name);
+        if (wheel.evenOdds)
+         {
+             set_even_odds();
+         }
+         else
+         {
+             cur->data->odds = odds;
+             wheel.remainingOdds -= odds;
+         } 
+    }
+    
 }
 
 struct Opt * roll()
@@ -73,28 +90,57 @@ struct Opt * roll()
     double winNum = (rand() % 100);
     struct Node *cur = wheel.head;
     double sum = 0.0;
+    printf("winning number: %f\n", winNum);
     while(cur != NULL)
     {
         if (winNum >= sum && winNum < (sum + cur->data->odds))
             return cur->data;
         else
+        {
+            sum += cur->data->odds;
             cur = cur->next;
+        }
     }
     return NULL;
 }
 
+void print_wheel()
+{
+    struct Node *cur = wheel.head;
+    while(cur != NULL)
+    {
+        printf("name: %s\todds: %f\n", cur->data->name, cur->data->odds);
+        cur = cur->next;
+    }
+}
+
+void clear_mem(struct Node *head)
+{
+    if (head != NULL)
+    {
+       clear_mem(head->next);
+
+        free(head->data->name);
+        free(head->data);
+        free(head);  
+    }
+}
+
 int main()
 {
-    srand( (unsigned) clock());
-    wheel.opts = 0;
+    struct Opt *winner;
     char even;
     char name[100];
     double odds;
     char more;
+    char rep;
+
+    srand( (unsigned) clock());
+    wheel.opts = 0;
 
     printf("Welcome to ScroreSettler!\n");
 
-    printf("Would you like to use even odds (y/n)\n");
+    printf("Would you like to use even odds? (y/n)\n");
     do 
     {
         scanf(" %c", &even);
@@ -116,39 +162,63 @@ int main()
     do
     {
         printf("name: ");
-        scanf(" %s", &name);
+        scanf(" %s", name);
         if (wheel.evenOdds)
-            add(name, 0.0);
+           {
+               add_opt(name, 0.0);
+           }
         else
         {
             int valid = 1;
-            printf("odds: ");
             do
             {
-                scanf(" %f", &odds);
+                printf("odds: ");
+                scanf(" %lf", &odds);
                 if (odds > wheel.remainingOdds || odds <= 0)
                 {
-                    printf("Invalid odds");
+                    printf("Invalid odds\n");
                     valid = 0;
                 }
+                else
+                {
+                    valid = 1;
+                }
+                
             }while (!valid);
-            add(name,odds);
+            add_opt(name,odds);
         }
-        printf("Would you like to add more options (y/n)\n");
+        printf("Would you like to add more options? (y/n)\n");
         do 
         {
             scanf(" %c", &more);
             if (more != 'y' && more != 'n')
             {
-                printf("Invalid option\n");
+                printf("Invalid input\n");
             }
         } while (more != 'n' && more != 'y');      
     } while (more == 'y');
     
-    printf("Rolling...\n");
-    struct Opt *winner = roll();
-    if (winner == NULL)
-        printf("No Winner!\n");
-    else
-        printf("%s was won with a chance of %f %%!", winner->name, winner->odds);
+    do {
+        printf("\nRolling...\n");
+        print_wheel();
+        winner = roll();
+
+        if (winner == NULL)
+            printf("No Winner!\n");
+        else
+            printf("%s was won with a chance of %f %%!\n", winner->name , winner->odds);
+        printf("\nWould you like to roll again? (y/n)\n");
+        do
+        {
+            scanf(" %c", &rep);
+            if (rep != 'y' && rep != 'n')
+            {
+                printf("Invalid Input\n");
+            }
+        } while (rep != 'y' && rep !='n');
+    
+    }while(rep == 'y');
+
+    clear_mem(wheel.head);
+    return 0;
 }
